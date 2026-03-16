b03761cc262d1cf29bd9a360669b8644f41a7b85
Shannon Besson <shannon0413@outlook.com>
2026-03-16 16:06:22 +0100

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import Navbar from "./components/navbar";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fr">
+    <html lang="fr" className={cn("font-sans", geist.variable)}>
       <body>
         <Navbar />
         {children}
